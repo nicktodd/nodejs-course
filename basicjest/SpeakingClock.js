c8eab681179de2cca844a4a0afa9833f54377400
Nick Todd <nick.todd@conygre.com>
2020-12-04 15:56:48 +0000

    Added a new calculator example

--- basicjest/SpeakingClock.js
+++ basicjest/SpeakingClock.js
@@ -4,8 +4,11 @@ module.exports = {
       if (time.getHours() == 0 && time.getMinutes() == 0) {
         return "midnight";
       }
+      else if (time.getHours() == 12 && time.getMinutes() == 0){
+          return "midday";
+      }
       else {
-          return null;
+        return null;
       }
     }
 }
