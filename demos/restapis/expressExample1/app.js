61595936b8e577c5fe4df290b6d7fa4561a23813
Nick Todd <nick.todd@conygre.com>
2025-12-16 14:34:11 +0000

    Added  basic CRUD HTML Pages

--- demos/restapis/expressExample1/app.js
+++ demos/restapis/expressExample1/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
+
+// Enable CORS to allow the HTML page to make requests to this API
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 const port = 5000;
 
 const users = [
@@ -38,6 +47,21 @@ app.post("/users", (req,res) => {
     res.status(201).json({message: message});
 });
 
+app.put("/users/:name", (req,res) => {
+    let matchingUserIndex = users.findIndex(user => user.name == req.params.name);
+    
+    if (matchingUserIndex !== -1) {
+        // Update the user with new data
+        users[matchingUserIndex] = req.body;
+        message = `Updated user ${req.body.name}`;
+        res.status(200).json({message: message});
+    }
+    else {
+        message = `No matching user ${req.params.name}`;
+        res.status(404).json({message: message});
+    }
+});
+
 app.delete("/users/:name", (req,res) => {
      let matchingUserIndex = users.findIndex(user => user.name == req.params.name );
     
@@ -52,8 +76,6 @@ app.delete("/users/:name", (req,res) => {
 
 
 })
-
-
 
 app.listen(port, () => {
     console.log("server running on port " + port);
